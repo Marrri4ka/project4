fa392c20f1cdc29e22c116fb330c5791716547f7
Mariia <mariiapopovych@gmail.com>
2019-04-12 12:36:59 -0700

    calculate price based on size

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,30 @@
 function Pizza(size, toppings) {
   this.size = size,
     this.toppings = toppings
-
 }
 
 
-
-
 Pizza.prototype.calculatePrice = function() {
-
   var price;
-  if (this.size === "mini") {
+  if (this.size === "small") {
     price = 9;
   }
-  if (this.size === "small") {
+  if (this.size === "medium") {
     price = 11;
   }
-  if (this.size === "medium") {
+  if (this.size === "large") {
     price = 13;
   }
-  if (this.size === "large") {
+  if (this.size === "x-large") {
     price = 16;
   }
-  return price;
+  for (var i = 0; i < toppingsArray.length; i++) {
+    if (toppingsArray[i] === "tomato") {
+      price = +2;
+
+    }
+    return price;
+  }
 }
 
 $(document).ready(function() {
@@ -56,15 +58,13 @@ $(document).ready(function() {
     if ($("#ham").is(":checked")) {
       toppingsArray.push("ham");
     }
-    console.log(toppingsArray);
+
 
 
 
     $("#final-price").text("You ordered " + size + " pizza with:" + toppingsArray);
     $("#final-price").show();
     var pizza = new Pizza(size, toppingsArray);
-
-
     $("#price").text("Pay: $" +
       pizza.calculatePrice());
     $("#price").show();
